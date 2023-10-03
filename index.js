07d946f9bd61b787b2d3f6febd73f77bf7f0d201
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-03 23:29:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+let userName = "Agent";
+let userEmail = "agent@gmail.com";
+let body = document.querySelector("body");
+
+const submitData = (userName, userEmail) => {
+    let userObj = {name: userName, email: userEmail}
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userObj)
+    })
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((resp) => {
+        body.innerHTML = `<p>${DataTransfer.id}</p>`;
+    })
+    .catch((error) => {
+        body.innerHTML = `${error.message}`;
+    });
+}
